e045bdd37cd55c0260f064bf3ff25c590777dbf7
Germanwin <grishingerman7766005@gmail.com>
2024-09-12 12:35:56 +0300

    made the footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import Navbar from './components/layout/navbar.vue';
+import Footer from './components/layout/footer.vue';
 </script>
 
 <template>
   <Navbar />
   <RouterView />
+  <Footer />
 </template>
---
+++ src/components/layout/footer.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import TitleBlockAnnouncement from '../main-view-components/blocks-components/title-block-components/title-block-announcement.vue';
+import SmLinks from './sm-links.vue';
+</script>
+
+<template>
+    <div class="footer-container">
+        <div class="left-column">
+            <a href="">Оплата</a>
+            <a href="">Отзывы</a>
+            <p>+7 911 777 8375</p>
+        </div>
+        <div class="middle-column">
+            <TitleBlockAnnouncement />
+        </div>
+        <div class="right-column">
+            <SmLinks />
+        </div>
+    </div>
+</template>
